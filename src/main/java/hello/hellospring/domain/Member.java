d3572f7ddb16aa4a06b861179a26d0a1c656d7e1
CHOIY3SUNG <choiyesung2848@naver.com>
2023-03-27 20:17:10 +0900

    Wowˆ

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,23 +1,9 @@
 package hello.hellospring.domain;
 
-public class Member {
-
+import lombok.Data;
+    @Data
+    public class Member {
     private Long id;
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
